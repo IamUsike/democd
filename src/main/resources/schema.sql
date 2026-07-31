0c338c6ad8ecaa072e5cbb78e96fa51fb69367a6
chiragtank749 <chiragtank749@gmail.com>
2026-07-31 07:03:19 +0000

    inital commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS booking (
     user_name    VARCHAR(100),
     workspace    VARCHAR(100)
 );
+
+CREATE TABLE IF NOT EXISTS user (
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    user_name    VARCHAR(100),
+    email    VARCHAR(100)
+);
---
+++ src/test/java/com/example/demo/repository/JdbcBookingRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@JdbcTest
+class JdbcBookingRepositoryTest {
+
+    @Test
+    void save() {
+    }
+}
